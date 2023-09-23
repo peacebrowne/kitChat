297bcd1ce79fd80cd226c1ec8b2fdfb1767176f4
peacebrowne <emmanuelbrowne751999@gmail.com>
2023-09-23 15:57:15 +0000

    Completed the html and css of the chat section

--- js/app.js
+++ js/app.js
@@ -12,4 +12,24 @@ document.addEventListener("click", (ev) => {
   targetElClas.includes("hambuger")
     ? toggle_class(element(".sidebar"), "slide-left")
     : remove_class(element(".sidebar"), "slide-left");
+
+  // SWITCHING SECTIONS
+  targetElClas.includes("nav-item")
+    ? console.log(targetEl, "included")
+    : console.log(targetEl, "not included");
 });
+
+/**
+ * Display a form base on user preference. Login or Registration.
+ * @param {class} ele - Current form to display
+ */
+function toggle_Sections(ele, clas) {
+  const current_form = ele.closest("form");
+  const next_form = element(`.${clas}-form`);
+
+  const inputs = elementAll(`.${current_form.className} input`);
+  reset(inputs);
+
+  add_class(current_form, "hide");
+  remove_class(next_form, "hide");
+}
